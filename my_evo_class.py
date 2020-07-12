0cf7fb5af84d17f774f9d31dca02483c588c4e09
Honglin Bao <baohongl@msu.edu>
2020-07-12 09:16:51 -0400

    Create my_evo_class.py

---
+++ my_evo_class.py
@@ -0,0 +1,54 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class Individual(ABC):
+    def __init__(self, value=None, init_params=None):
+        if value is not None:
+            self.value = value
+        else:
+            self.value = self._random_init(init_params)
+
+    @abstractmethod
+    def pair(self, other, pair_params):
+        pass
+
+    @abstractmethod
+    def mutate(self, mutate_params):
+        pass
+
+    @abstractmethod
+    def _random_init(self, init_params):
+        pass
+
+
+class Optimization(Individual):
+    def pair(self, other, pair_params):
+        return Optimization(pair_params['alpha'] * self.value + (1 - pair_params['alpha']) * other.value)
+
+    def mutate(self, mutate_params):
+        self.value += np.random.normal(0, mutate_params['rate'], mutate_params['dim'])
+        for i in range(len(self.value)):
+            if self.value[i] < mutate_params['lower_bound']:
+                self.value[i] = mutate_params['lower_bound']
+            elif self.value[i] > mutate_params['upper_bound']:
+                self.value[i] = mutate_params['upper_bound']
+
+    def _random_init(self, init_params):
+        return np.random.uniform(init_params['lower_bound'], init_params['upper_bound'], init_params['dim'])
+
+
+class Population:
+    def __init__(self, size, fitness, individual_class, init_params):
+        self.fitness = fitness
+        self.individuals = [individual_class(init_params=init_params) for _ in range(size)]
+        self.individuals.sort(key=lambda x: self.fitness(x))
+
+    def replace(self, new_individuals):
+        size = len(self.individuals)
+        self.individuals.extend(new_individuals)
+        self.individuals.sort(key=lambda x: self.fitness(x))
+        self.individuals = self.individuals[-size:]
+
+    #def get_parents(self, n_offsprings):
+       
