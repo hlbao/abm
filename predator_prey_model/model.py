7e39744f5b34738dd5cec25267a1647ade207b23
Honglin Bao <baohongl@msu.edu>
2020-07-26 17:10:26 -0400

    Create model.py

---
+++ predator_prey_model/model.py
@@ -0,0 +1,57 @@
+"""
+Wolf-Sheep Predation Model
+================================
+Replication of the model found in NetLogo:
+    Wilensky, U. (1997). NetLogo Wolf Sheep Predation model.
+    http://ccl.northwestern.edu/netlogo/models/WolfSheepPredation.
+    Center for Connected Learning and Computer-Based Modeling,
+    Northwestern University, Evanston, IL.
+"""
+
+from mesa import Model
+from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+
+from wolf_sheep.agents import Sheep, Wolf, GrassPatch
+from wolf_sheep.schedule import RandomActivationByBreed
+
+
+class WolfSheep(Model):
+    """
+    Wolf-Sheep Predation Model
+    """
+
+    height = 20
+    width = 20
+
+    initial_sheep = 100
+    initial_wolves = 50
+
+    sheep_reproduce = 0.04
+    wolf_reproduce = 0.05
+
+    wolf_gain_from_food = 20
+
+    grass = False
+    grass_regrowth_time = 30
+    sheep_gain_from_food = 4
+
+    verbose = False  # Print-monitoring
+
+    description = (
+        "A model for simulating wolf and sheep (predator-prey) ecosystem modelling."
+    )
+
+    def __init__(
+        self,
+        height=20,
+        width=20,
+        initial_sheep=100,
+        initial_wolves=50,
+        sheep_reproduce=0.04,
+        wolf_reproduce=0.05,
+        wolf_gain_from_food=20,
+        grass=False,
+        grass_regrowth_time=30,
+        sheep_gain_from_food=4,
+    ):
