7d63dc7f1c2e83a956a0cf0e3e98d9589b02b4a8
Honglin Bao <baohongl@msu.edu>
2020-08-02 10:48:11 -0400

    Create server.py

---
+++ predator_prey_model/server.py
@@ -0,0 +1,40 @@
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid, ChartModule
+from mesa.visualization.UserParam import UserSettableParameter
+
+from wolf_sheep.agents import Wolf, Sheep, GrassPatch
+from wolf_sheep.model import WolfSheep
+
+
+def wolf_sheep_portrayal(agent):
+    if agent is None:
+        return
+
+    portrayal = {}
+
+    if type(agent) is Sheep:
+        portrayal["Shape"] = "wolf_sheep/resources/sheep.png"
+        # https://icons8.com/web-app/433/sheep
+        portrayal["scale"] = 0.9
+        portrayal["Layer"] = 1
+
+    elif type(agent) is Wolf:
+        portrayal["Shape"] = "wolf_sheep/resources/wolf.png"
+        # https://icons8.com/web-app/36821/German-Shepherd
+        portrayal["scale"] = 0.9
+        portrayal["Layer"] = 2
+        portrayal["text"] = round(agent.energy, 1)
+        portrayal["text_color"] = "White"
+
+    elif type(agent) is GrassPatch:
+        if agent.fully_grown:
+            portrayal["Color"] = ["#00FF00", "#00CC00", "#009900"]
+        else:
+            portrayal["Color"] = ["#84e184", "#adebad", "#d6f5d6"]
+        portrayal["Shape"] = "rect"
+        portrayal["Filled"] = "true"
+        portrayal["Layer"] = 0
+        portrayal["w"] = 1
+        portrayal["h"] = 1
+
+    return portrayal
